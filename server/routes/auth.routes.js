e48b7569d321bb489511c5284b53b3113d5b2e0a
bishwa bandhu parmar <bishwabandhuparmar06@gmail.com>
2025-08-16 00:54:45 +0530

    Password Reset Link

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const {upload} = require("../utils/uploads/multer");
+const { upload } = require("../utils/uploads/multer");
 
-const { registerUsers, loginWithPassword,loginWithEmailOtp, verifyLoginotp,registerSeller,registerAdmin,validateOtp , logout} = require("../controllers/auth.Controller");
+const {
+  registerUsers,
+  loginWithPassword,
+  forgetPassword,
+  verifyResetOtp,
+  resetPassword,
+  loginWithEmailOtp,
+  verifyLoginotp,
+  registerSeller,
+  registerAdmin,
+  validateOtp,
+  logout,
+} = require("../controllers/auth.Controller");
 
 router.post("/register-users", registerUsers);
-router.post("/register-sellers",upload.single("gst_Certificate"), registerSeller);
+router.post(
+  "/register-sellers",
+  upload.single("gst_Certificate"),
+  registerSeller
+);
 router.post("/register-admin", registerAdmin);
 router.post("/login-password", loginWithPassword);
 router.post("/login-with-emailOtp", loginWithEmailOtp);
 
-router.post("/validate-otp", validateOtp)
-router.post("/validate-loginotp", verifyLoginotp)
-router.post("/logout", logout)
+router.post("/validate-otp", validateOtp);
+router.post("/validate-loginotp", verifyLoginotp);
+router.post("/logout", logout);
+router.post("/forget-password", forgetPassword);
+router.post("/verify-reset-otp", verifyResetOtp);
+router.post("/reset-password", resetPassword);
 module.exports = router;
