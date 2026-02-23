acc3d203e984f1c9061103cfada4a2cef3e04637
bishwa bandhu parmar <bishwabandhuparmar06@gmail.com>
2026-02-23 09:25:53 +0530

    Added

--- client/src/API/axiosInstance.js
+++ client/src/API/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 // console.log("Checking URl : ", import.meta.env.VITE_API_URL);
 // Create an Axios instance
 const api = axios.create({
-  // baseURL: "/api",
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "/api",
+  // baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
